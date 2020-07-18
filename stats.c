02cb799f413c6dfe5e99a9071235ff9d363223a8
John Hageman <jmhageman7@gmail.com>
2020-07-17 23:08:09 -0500

    Adding code outline and documentation for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -28,16 +28,49 @@
 #define SIZE (40)
 
 void main() {
-
+	
+  /* Variable Declarations */
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+                                7,  87, 250, 230,  99,   3, 100,  90};  
+  unsigned int n_test = SIZE;
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+
+
+
+  /* Statistics and Printing Functions */
+
+  void print_statistics(){
+	  //TODO: add code
+  }
+
+  void print_array(unsigned char * array[], unsigned int n){
+	  //TODO: add code
+  }
+
+  unsigned char find_median(unsigned char * array[], unsigned int n){
+	  //TODO: add code
+  }
+
+  unsigned char find_mean(unsigned char * array[], unsigned int n){
+	  //TODO: add code
+  }
+
+  unsigned char find_maximum(unsigned char * array[], unsigned int n){
+	  //TODO: add code
+  }
+
+  unsigned char find_minimum(unsigned char * array[], unsigned int n){
+	  //TODO: add code
+  }
+
+  void sort_array(unsigned char * array[], unsigned int n){
+	  //TODO: add code
+  }
 
 }
 
 /* Add other Implementation File Code Here */
+
--- stats.h
+++ stats.h
@@ -19,22 +19,93 @@
  *
  */
 #ifndef __STATS_H__
-#define __STATS_H__
-
-/* Add Your Declarations and Function Comments here */ 
+#define __STATS_H__ 
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Prints the statistics of an array
  *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * This function prints minimum, maximum, mean, and median
+ * of the array of data being analyzed.
  */
+void print_statistics();
+
+
+/**
+ * @brief Prints the array to the screen
+ *
+ * Given an array of data and a length,
+ * this function prints the array to the screen
+ *
+ * @param array Array of numbers to get printed
+ * @param n The number of elements in array
+ */
+void print_array(unsigned char * array[], unsigned int n);
+
+
+/**
+ * @brief Returns the median value of an array
+ *
+ * Given an array of data and a length, returns the median value
+ *
+ * @param array Array of numbers to analyze
+ * @param n The number of elements in array
+ *
+ * @return The median of the array
+ */
+unsigned char find_median(unsigned char * array[], unsigned int n);
+
+
+/**
+ * @brief Returns the mean value of an array
+ *
+ * Given an array of data and a length, returns the mean value
+ *
+ * @param array Array of numbers to analyze
+ * @param n The number of elements in array
+ *
+ * @return The mean of the array
+ */
+unsigned char find_mean(unsigned char * array[], unsigned int n);
+
+
+/**
+ * @brief Returns the maximum value of an array
+ *
+ * Given an array of data and a length, returns the maximum value
+ *
+ * @param array Array of numbers to analyze
+ * @param n The number of elements in array
+ *
+ * @return The maximum of the array
+ */
+unsigned char find_maximum(unsigned char * array[], unsigned int n);
+
+
+/**
+ * @brief Returns the minimum value of an array
+ *
+ * Given an array of data and a length, returns the minimum value
+ *
+ * @param array Array of numbers to analyze
+ * @param n The number of elements in array
+ *
+ * @return The minimum of the array
+ */
+unsigned char find_minimum(unsigned char * array[], unsigned int n);
+
+
+/**
+ * @brief Sorts the input array from largest to smallest
+ *
+ * Given an array of data and a length, sorts the array from 
+ * largest to smallest. (The zeroth Element should be the
+ * largest value, and the last element (n-1) should be the 
+ * smallest value. )
+ *
+ * @param array Array of numbers to sort
+ * @param n The number of elements in array
+ */
+void sort_array(unsigned char * array[], unsigned int n);
 
 
 #endif /* __STATS_H__ */
